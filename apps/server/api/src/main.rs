e696a98318a52ad9093baaa073898e1cf443f01b
frolleks <frolleks@hotmail.com>
2024-04-27 13:13:10 +0700

    chore(deps-api): add axum and tokio

--- apps/server/api/src/main.rs
+++ apps/server/api/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(|| async { Html("<p>hello world</p>") }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
